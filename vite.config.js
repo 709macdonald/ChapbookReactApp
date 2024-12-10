113c4ad4eef966a6bba1b7cda2ecbd5cf5d1ea2e
709macdonald <709macdonald@gmail.com>
2024-12-10 10:13:38 -0330

    PDF worker works on own server

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/ChapbookReactApp/",
   define: {
     global: "window",
   },
@@ -13,3 +14,4 @@ export default defineConfig({
     },
   },
 });
+3;
